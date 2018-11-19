b78b214af412cc5cb4a1a97477008378d9feb3ad
Helen <alenka352331@gamil.com>
2018-11-19 18:40:33 +0200

    order_validation

--- lib/models/order.rb
+++ lib/models/order.rb
@@ -1,13 +1,21 @@
 # frozen_string_literal: true
 
 class Order
-  attr_accessor :book
-  attr_accessor :reader
-  attr_accessor :date
+  include Validator
+  attr_reader :book, :reader, :date
 
-  def initialize(book = nil, reader = nil, date = nil)
+  def initialize(book, reader, date)
+    validate(book, reader, date)
     @book = book
     @reader = reader
     @date = date
+  end
+
+  private
+
+  def validate(book, reader, date)
+    check_class(book, Book)
+    check_class(reader, Reader)
+    check_class(date, Time)
   end
 end
