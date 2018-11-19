b30b7672aeb95cc9fdfff641cadfa44aab1dc41b
Helen <alenka352331@gamil.com>
2018-11-19 18:28:54 +0200

    book_validation

--- lib/models/book.rb
+++ lib/models/book.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
 class Book
-  attr_accessor :title
-  attr_accessor :author
+  include Validator
+  attr_reader :title, :author
 
-  def initialize(title = nil, author = nil)
+  def initialize(title, author)
+    validate(title, author)
     @title = title
     @author = author
   end
+
+  private
+
+   def validate(title, author)
+    check_class(title, String)
+    check_class(author, Author)
+    check_empty(title)
+  end
+  
 end
