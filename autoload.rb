1ab987737eb437442615f5f63b04eeb6c04aab06
hel352331 <31447813+hel352331@users.noreply.github.com>
2018-11-19 21:02:54 +0200

    Delete autoload.rb

--- autoload.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'faker'
-require_relative 'validator'
-require_relative 'lib/models/author'
-require_relative 'lib/models/book'
-require_relative 'lib/models/reader'
-require_relative 'lib/models/order'
-require_relative 'lib/models/library'
